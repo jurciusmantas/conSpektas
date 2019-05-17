c726a5637456e3a96500a45e0ddb061ddec31abb
Žygimantas Stongvilas <stongvilas.zygimantas@gmail.com>
2019-05-18 00:31:09 +0300

    Working local db

--- conSpektas.Application/Startup.cs
+++ conSpektas.Application/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using conSpektas.Domain;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,6 +21,8 @@ namespace conSpektas.Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<ConspectContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConSpektasConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- conSpektas.Domain/ConspectContext.cs
+++ conSpektas.Domain/ConspectContext.cs
@@ -7,11 +7,15 @@ namespace conSpektas.Domain
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Conspect> Conspects { get; set; }
-        public DbSet<ConspectRating> ConspectRatings { get; set; }
+        public DbSet<ConspectRating> ConspectsRatings { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<CommentRating> CommentRatings { get; set; }
+        public DbSet<CommentRating> CommentsRatings { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ConspectCategory> ConspectsCategories { get; set; }
+
+        public ConspectContext(DbContextOptions<ConspectContext> options)
+            : base(options)
+        { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -32,13 +36,17 @@ namespace conSpektas.Domain
             modelBuilder.Entity<ConspectCategory>()
                 .HasKey(category => new { category.CategoryId, category.ConspectId });
 
+            modelBuilder.Entity<Comment>()
+                .HasOne(comment => comment.User)
+                .WithMany(user => user.Comments).OnDelete(DeleteBehavior.Restrict);
+
             modelBuilder.Entity<Conspect>()
                 .HasOne(conspect => conspect.Parent)
                 .WithOne().OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Conspect>()
                 .HasOne(conspect => conspect.User)
-                .WithMany().OnDelete(DeleteBehavior.Restrict);
+                .WithMany(user => user.Conspects).OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
